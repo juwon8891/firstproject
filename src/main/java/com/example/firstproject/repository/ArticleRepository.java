8e368bb08d8ec31133aa6f870bd932f08c82441f
kevin <juwon8891@gmail.com>
2022-05-10 16:50:43 +0900

    h2 DB, 웹 콘솔 접근 허용

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -4,6 +4,4 @@ import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleRepository extends CrudRepository<Article,Long> {
-
-
 }

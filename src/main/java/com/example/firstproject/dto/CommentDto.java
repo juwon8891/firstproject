08c0943e4baca8f0aefa38be80421ba9495a3430
juwon <juwon8891@gmail.com>
2022-06-06 23:20:30 +0900

    23 댓글 서비스와 컨트롤러(feat. REST API)

--- src/main/java/com/example/firstproject/dto/CommentDto.java
+++ src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -1,2 +1,31 @@
-package com.example.firstproject.dto;public class CommentDto {
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+    private Long id;
+    @JsonProperty("article_id")
+    private Long articleId;
+    private String nickname;
+    private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getBody(),
+                comment.getNickname()
+        );
+    }
 }
